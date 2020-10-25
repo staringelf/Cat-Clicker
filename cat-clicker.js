0d6c18130e39d281af72009827ecf72685b3704d
Manish <msoffice37@gmail.com>
2020-10-25 08:12:47 -0700

    will stucture MVO

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,41 +1,91 @@
-const catNames = document.querySelectorAll('.cat-name');
-const cat = document.querySelector('.cat');
+const model = {
+	cats: [
+		{
+			name: 'Dorothy',
+			src: 'dorothy.jpg',
+			clicks: 0,
+		},
+		
+		{
+			name: 'Sheena',
+			src: 'sheena.jpg',
+			clicks: 0,
+		},
+		
+		{
+			name: 'Alexandra',
+			src: 'alexandra.jpg',
+			clicks: 0,
+		},
+		
+		{
+			name: 'Slavy',
+			src: 'slavy.jpg',
+			clicks: 0,
+		},
+		
+		{
+			name: 'Eva',
+			src: 'eva.jpg',
+			clicks: 0,
+		}
+	]
+};
 
-const displayCatName = document.querySelector('.cat__name');
-const displayCatImg = document.querySelector('.cat__img');
-const displayCatClicks = document.querySelector('.cat__clicks');
+const octopus = {
+	
+	getCats: function () {
+		return model.cats;
+	},
+	
+	displayCat: function (cat) {
+		viewDisplay.render(cat);
+	},
+	
+	incrementClicks: function (cat) {
+		cat.clicks++;
+		viewDisplay.render(cat);
+	},
+	
+	init: function () {
+		viewButtons.init();
+	}
+	
+};
 
-let clicks = 0;
-
-function Cat(name, img) {
-	this.name = name;
-	this.img = img;
+const viewButtons = {
+	init: function () {
+		const catsUl = document.createElement('ul');
+		octopus.getCats().forEach(cat => {
+			const catLi = document.createElement('li');
+			catLi.innerHTML = `<button class="cat-button">${cat.name}</button>`;
+			catLi.classList.add('cats-nav__item');
+			catLi.addEventListener('click', () => {
+				octopus.displayCat(cat);
+			});
+			catsUl.appendChild(catLi);
+		})
+		document.querySelector('.cat-buttons').appendChild(catsUl);
+	},
 }
 
-catNames.forEach(catName => catName.addEventListener('click', function () {
-	const cat = new Cat(catName.textContent, `${catName.textContent}.jpg`);
-	console.log(cat);
-	[displayCatName.textContent, displayCatImg.src] = [cat.name, cat.img];
-	clicks = 0;
-	displayCatClicks.textContent = clicks;
-}))
-
-displayCatImg.addEventListener('click', function  () {
-	displayCatClicks.textContent = ++clicks;
-})
-
-
-/*const cats = document.querySelectorAll('.cat_image');
-const catNames = ['eva', 'dorothy'];
-
-let score = [0, 0];
-
-cats.forEach(function (cat, index){
-	cat.parentNode.querySelector('.cat__name').textContent = catNames[index];
+const viewDisplay = {
+	render: function (cat) {
+		const catDisplay = document.querySelector('.cat-display');
+		const catContainer = catDisplay.querySelector('.cat-container');
+		catContainer.innerHTML = `<h3 class="cat__name">${cat.name}</h3>
+								  <p class="cat__clicks">${cat.clicks}</p>`;
+		const catFigure = document.createElement('figure');
+		catFigure.classList.add('cat__photo');
+		catFigure.addEventListener('click', () => {
+			octopus.incrementClicks(cat);
+		});
+		catFigure.innerHTML = `<img class="cat__img" src="Images/${cat.src}" alt="">`
+		catContainer.appendChild(catFigure);
+		catDisplay.style.display = 'block';
+	},
 	
-	cat.addEventListener('click', function () {
-	const scoreBoard = cat.parentNode.querySelector('.scoreboard');
-	scoreBoard.textContent = ++score[index];
-	})
-})
-*/
+	
+}
+
+octopus.init();
